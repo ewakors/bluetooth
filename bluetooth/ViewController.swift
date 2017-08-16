5c2a6a6f2e798e1635b26b035b4242e912125a26
ewakorszaczuk <ewakorszaczuk@tjsodtware.co>
2017-08-16 09:30:39 +0200

    Initial Commit

---
+++ bluetooth/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  bluetooth
+//
+//  Created by Ewa Korszaczuk on 16.08.2017.
+//  Copyright © 2017 Ewa Korszaczuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
